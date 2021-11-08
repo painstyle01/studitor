cf8e3fc41d40816bcdf3ff653e9343f4d1c29111
Maxim Shelyhin <31153936+painstyle01@users.noreply.github.com>
2021-11-08 12:31:20 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
-token = '1289160930:AAEkWIGyjqZ2j7PKj-sxp4lw2p9gf3e4yxA'
-# Sandbox = '1296737129:AAEwPZXpQNXetvJxFvejvUwpFm_Pujw4iOg'
+token = '$$$'
+Sandbox = '$$$'
 
-db_host = 'zv390787.mysql.tools'
-db_user = 'zv390787_bot'
-db_passwd = '8Y#Vn#8u9b'
-db_base = 'zv390787_bot'
-webhook_host = '185.65.244.165'
-admins = -1001470150298
+db_host = '$$$'
+db_user = '$$$'
+db_passwd = '$$$'
+db_base = '$$$'
+webhook_host = '$$$'
+admins = [$$$]
 
